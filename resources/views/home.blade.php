066ec39aff2da2bc5019d968efaa34b4b40404e4
steveenadominguezb <steveen.dominguez@24-7intouch.com>
2022-04-26 08:29:44 -0500

    adding a modal to create new wave

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,10 @@
                         {{ __('Dashboard') }}
                     </div>
                     <div class="d-inline-block text-end" style="width: 49%;">
-                        <button class="fw-bold fs-3" style="border: none; background: none;">+</button>
+                        <button class="fw-bold fs-3 " style="border: none; background: none;" data-bs-toggle="modal" data-bs-target="#exampleModal">+</button>
+                        
                     </div>
+
 
                 </div>
 
@@ -23,12 +25,12 @@
                     @endif
 
                     @foreach ($waves as $wave)
-                    
 
-                    <div class="card d-inline-block" style="width: 18rem;">
+
+                    <div class="card d-inline-block me-5" style="width: 15rem;">
                         <img src="img/{{$wave->programs->img}}" class="card-img-top" alt="...">
                         <div class="card-body">
-                        
+
                             <p class="card-text fw-bold">{{$wave->programs->Name}}</p>
                             <p class="card-text fw-bold">{{ $wave->Name}}</p>
                         </div>
@@ -48,6 +50,25 @@
                     </div> -->
 
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
