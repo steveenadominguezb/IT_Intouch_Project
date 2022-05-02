215deee07e754ce9db1b7510dfce7ebabf874e25
steveenadominguezb <steveen.dominguez@24-7intouch.com>
2022-05-02 11:35:33 -0500

    update programs with photo

--- database/seeders/ProgramsTableSeeder.php
+++ database/seeders/ProgramsTableSeeder.php
@@ -18,16 +18,19 @@ class ProgramsTableSeeder extends Seeder
         $program1 = new Program();
         $program1->IdProgram = 101;
         $program1->Name = "TCP";
+        $program1->img = "tcp.png";
         $program1->save();
 
         $program2 = new Program();
         $program2->IdProgram = 102;
         $program2->Name = "GNC";
+        $program2->img = "gnc.png";
         $program2->save();
 
         $program3 = new Program();
         $program3->IdProgram = 103;
         $program3->Name = "Optavia";
+        $program3->img = "optavia.jpg";
         $program3->save();
 
         $program4 = new Program();
@@ -45,6 +48,7 @@ class ProgramsTableSeeder extends Seeder
         $program6 = new Program();
         $program6->IdProgram = 106;
         $program6->Name = "L'Oreal";
+        $program6->img = "l'oreal.jpg";
         $program6->save();
 
         $program7 = new Program();
@@ -56,16 +60,19 @@ class ProgramsTableSeeder extends Seeder
         $program8 = new Program();
         $program8->IdProgram = 108;
         $program8->Name = "Vroom";
+        $program8->img = "vroom.jpg";
         $program8->save();
 
         $program9 = new Program();
         $program9->IdProgram = 109;
         $program9->Name = "Weber";
+        $program9->img = "weber.jpg";
         $program9->save();
 
         $program10 = new Program();
         $program10->IdProgram = 110;
         $program10->Name = "Levis";
+        $program10->img = "levis.png";
         $program10->save();
     }
 }
